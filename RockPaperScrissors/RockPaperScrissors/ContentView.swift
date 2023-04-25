e59e82b2a87ac5f20b7761b3034ba865b0b5eb3c
Berardino Chiarello <berardino95@gmail.com>
2023-04-25 20:10:47 +0200

    All function implemented

--- RockPaperScrissors/RockPaperScrissors/ContentView.swift
+++ RockPaperScrissors/RockPaperScrissors/ContentView.swift
@@ -8,16 +8,179 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let moves = ["🪨", "📄", "✂️"]
+    @State private var userChoice : String = ""
+    @State private var appChoice : String = ""
+    @State private var alertText : String = ""
+    @State private var userScore: Int = 0
+    @State private var appScore: Int = 0
+    @State private var round: Int = 1
+    var remainingCoin: Int { 8 - round + 1 }
+    @State private var alertIsShowed : Bool = false
+    @State private var endAlertIsShowed : Bool = false
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack (spacing:0){
+            Spacer()
+            
+            Text("Rocks paper\nscissors")
+                .font(.largeTitle)
+                .multilineTextAlignment(.center)
+            
+            Spacer()
+            
+            HStack{
+                Spacer()
+                VStack{
+                    Text("Your choice")
+                    Text(userChoice)
+                        .emoji()
+                }
+                Spacer()
+                VStack{
+                    Text("Pc choice")
+                    Text(appChoice)
+                        .emoji()
+                }
+                
+                Spacer()
+            }
+            
+            Spacer ()
+            
+            VStack{
+                Text("Select your move")
+                HStack{
+                    ForEach(moves, id: \.self){ move in
+                        Button(move){
+                            check(move)
+                        }
+                        .buttonStyle(moveButton())
+                    }
+                }
+                .padding()
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(.green, lineWidth: 2)
+                )
+            }
+            
+            Spacer()
+            
+            VStack(spacing: 10){
+                Text("Your Score: \(userScore)")
+                Text("Pc Score: \(appScore)")
+            }
+            
+            Spacer()
+            
+            Text("Remaining coin: \(remainingCoin)")
+                .font(.caption)
         }
         .padding()
+        
+        .alert(alertText, isPresented: $alertIsShowed) {
+            Button("ok", role: .cancel, action: nextRound)
+        } message: {
+            Text("Your score: \(userScore)\nPc Score: \(appScore)")
+        }
+        
+        .alert(alertText, isPresented: $endAlertIsShowed) {
+            Button("Restart", role: .cancel, action: restart)
+        } message: {
+            Text("Final score \n You: \(userScore)  Pc: \(appScore)")
+        }
+        
+    }
+    
+    func restart() {
+        userChoice = ""
+        appChoice = ""
+        userScore = 0
+        appScore = 0
+        round = 1
+    }
+    
+    func nextRound() {
+        userChoice = ""
+        appChoice = ""
+    }
+    
+    func check(_ move: String){
+        appChoice = moves.randomElement()!
+        userChoice = move
+        
+        if round == 8 {
+            endAlertIsShowed = true
+        } else {
+            alertIsShowed = true
+        }
+        switch (userChoice, appChoice) {
+        case ("📄","🪨") :
+            alertText = "You won"
+            userScore += 1
+            round += 1
+        case ("✂️","📄") :
+            alertText = "You won"
+            userScore += 1
+            round += 1
+        case ("🪨","✂️") :
+            alertText = "You won"
+            userScore += 1
+            round += 1
+        case ("🪨","📄") :
+            alertText = "You lose"
+            appScore += 1
+            round += 1
+        case ("📄","✂️") :
+            alertText = "You lose"
+            appScore += 1
+            round += 1
+        case ("✂️","🪨") :
+            alertText = "You lose"
+            appScore += 1
+            round += 1
+        default:
+            alertText = "Draw"
+            round += 1
+        }
     }
 }
+
+
+//custom button style
+struct moveButton: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .font(.system(size: 30))
+            .frame(width: 60, height: 60)
+            .background(Color(.systemGray4))
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+}
+
+
+// Custom Text modifier for bigger icon
+struct Emoji : ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.system(size: 90))
+            .frame(width: 140, height: 140)
+            .overlay(
+                RoundedRectangle(cornerRadius: 20)
+                    .stroke(.green, lineWidth: 2)
+            )
+    }
+}
+
+extension View {
+    func emoji() -> some View {
+        modifier(Emoji())
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
