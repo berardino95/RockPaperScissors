036b6b54e0bb743dacb64815c7214d562af2f85c
Berardino Chiarello <berardino95@gmail.com>
2023-04-25 18:06:54 +0200

    Starting project

---
+++ RockPaperScrissors/RockPaperScrissors/RockPaperScrissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScrissorsApp.swift
+//  RockPaperScrissors
+//
+//  Created by CHIARELLO Berardino - ADECCO on 25/04/23.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScrissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
